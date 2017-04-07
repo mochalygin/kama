f15ab515d5ef7a1957fec0283b8ba086733a63a0
mochalygin <a@mochalygin.ru>
2017-04-07 22:33:27 +0300

    ADD Kama::getMessages()

--- src/AppBundle/Service/Kama.php
+++ src/AppBundle/Service/Kama.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace AppBundle\Service;
 
 
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManager;
 use AppBundle\Entity\Fren;
 use AppBundle\Entity\Message;
@@ -83,8 +84,8 @@ class Kama
             throw new \Exception('Message UUID must have 36 digits length');
         }
 
-        if (! $this->checkSign($fromKey, $uuid . $cryptedData, $sign)) {
-            throw new \Exception('Bad sign(' . $sign . ') for key(' . $fromKey . '), message(' . $uuid . '): ' . $cryptedData);
+        if (! $this->checkSign($fromFren->getKey(), $uuid . $cryptedData, $sign)) {
+            throw new \Exception('Bad sign(' . $sign . ') for key(' . $fromFren->getKey() . '), message(' . $uuid . '): ' . $cryptedData);
         }
 
         $message = new Message;
@@ -97,6 +98,38 @@ class Kama
         $this->em->flush();
 
         return true;
+    }
+
+    /**
+     * @param string $frenKey
+     * @param \DateTime $fromDateTime
+     * @param string $sign
+     *
+     * @return array|Message[]
+     *
+     * @todo украв 1 раз запрос с подписью -- можно вечно читать всё новые сообщения, неопрядок
+     */
+    public function getMessages(string $frenKey, \DateTime $fromDateTime, string $sign)
+    {
+        if (! $this->checkSign($frenKey, $fromDateTime->format('Y-m-d H:i:s'), $sign)) {
+            throw new \Exception('Bad sign(' . $sign . ') for key(' . $frenKey . '), datetime: ' . $fromDateTime->format('Y-m-d H:i:s'));
+        }
+
+        $fren = $this->em->getRepository(Fren::class)->findOneByKey($frenKey);
+        if (! $fren) {
+            throw new \Exception('This Kama do not know Fren with key: ' . $frenKey);
+        }
+
+        $timestamp = new \DateTime;
+
+        $messages = $this->em->getRepository(Message::class)->matching(
+            Criteria::create()
+                ->where(Criteria::expr()->eq('to', $fren))
+                ->andWhere(Criteria::expr()->gte('createdAt', $fromDateTime))
+                ->andWhere(Criteria::expr()->lte('createdAt', $timestamp))
+        );
+
+        return ['messages' => $messages, 'timestamp' => $timestamp->format('Y-m-d H:i:s')];
     }
 
     /**
